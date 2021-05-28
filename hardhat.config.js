b13b5a46ad65cdb75dbb21e211d383826d8f700b
johnfr14 <jonathan.alyra@gmail.com>
2021-05-28 15:51:50 +0200

    ICO start

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable semi */
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-solhint');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.4',
+}
